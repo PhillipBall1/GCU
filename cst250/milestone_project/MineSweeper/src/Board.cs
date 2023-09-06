9291058883ba473f8d11a40f811aee7a4f6f406e
PhillipBall1 <68392878+PhillipBall1@users.noreply.github.com>
2023-09-05 21:42:30 -0700

    Testing out functions for the actual playable version | cst250

--- cst250/milestone_project/MineSweeper/src/Board.cs
+++ cst250/milestone_project/MineSweeper/src/Board.cs
@@ -21,12 +21,12 @@ public class Board
 	public int[] GetSize() { return size; }
 
 
-    public void SetupLiveNeighbors()
+	public void SetupLiveNeighbors()
 	{
 
 		int cellCount = size[0] * size[1];
-        //set the cell array equal to the width * height of the board
-        cells = new Cell[cellCount];
+		//set the cell array equal to the width * height of the board
+		cells = new Cell[cellCount];
 
 		//x for loop
 		for(int i = 0; i < size[0]; i++)
@@ -53,13 +53,13 @@ public class Board
 		start++;
 	}
 
-    //start for loop for each square to be tested
-    private void CalculateLiveNeighbors()
+	//start for loop for each square to be tested
+	private void CalculateLiveNeighbors()
 	{
 		for(int i = 0; i < cells.Length; i++)
 		{
 			CheckSquares(cells[i]);
-        }
+		}
 	}
 
 	//checks the possibility from each square surrounding the initial square
@@ -67,43 +67,84 @@ public class Board
 	{
 		int liveCount = 0;
 
-        //each possible pos for row -1
-        liveCount += FindCellInPos(cell, -1, -1);
-		liveCount += FindCellInPos(cell, -1, 0);
-		liveCount += FindCellInPos(cell, -1, 1);
+		//each possible pos for row -1
+		liveCount += FindLiveCells(cell, -1, -1);
+		liveCount += FindLiveCells(cell, -1, 0);
+		liveCount += FindLiveCells(cell, -1, 1);
 
 		//each possible pos for row 0
-        liveCount += FindCellInPos(cell, 0, -1);
-        liveCount += FindCellInPos(cell, 0, 1);
+		liveCount += FindLiveCells(cell, 0, -1);
+		liveCount += FindLiveCells(cell, 0, 1);
 
 		//each possible pos for row 1
-        liveCount += FindCellInPos(cell, 1, -1);
-        liveCount += FindCellInPos(cell, 1, 0);
-        liveCount += FindCellInPos(cell, 1, 1);
+		liveCount += FindLiveCells(cell, 1, -1);
+		liveCount += FindLiveCells(cell, 1, 0);
+		liveCount += FindLiveCells(cell, 1, 1);
 
 		//check the starting cell
 		if(cell.GetLive()) liveCount += 1;
 
 		//set the live neighbors on the cell (min: 0, max: 9)
 		cell.SetLiveNeighbors(liveCount);
-    }
+	}
 
-	//finds the cell in the designated pos 
-	private int FindCellInPos(Cell cell, int posX, int posY)
+	//finds the cell in the designated pos returns int if it is live
+	private int FindLiveCells(Cell startCell, int posX, int posY)
 	{
-        for (int i = 0; i < cells.Length; i++)
-        {
+		for (int i = 0; i < cells.Length; i++)
+		{
 
-            if(cells[i].GetColumn() == cell.GetColumn() - posY && cells[i].GetRow() == cell.GetRow() - posX)
+			if(cells[i].GetColumn() == startCell.GetColumn() - posY && cells[i].GetRow() == startCell.GetRow() - posX)
 			{
-                // found a cell in set pos and check live
-                if (cells[i].GetLive())
+				// found a cell in set pos and check live
+				if (cells[i].GetLive())
 				{
 					return 1;
 				}
 			}
-        }
+		}
 		//did not find a cell in set pos
-        return 0;
+		return 0;
 	}
+	
+	
+	public int RevealCells(Cell startCell)
+	{
+		if(startCell.GetLive()) return -1;
+		
+		
+		if(!startCell.GetVisited() && startCell.GetLiveNeighbors() == 0)
+		{
+			startCell.SetVisited(true);
+			RevealCells(FindCellInPos(startCell, -1, -1));
+			RevealCells(FindCellInPos(startCell, -1,  0));
+			RevealCells(FindCellInPos(startCell, -1,  1));
+			
+			RevealCells(FindCellInPos(startCell,  1, -1));
+			RevealCells(FindCellInPos(startCell,  1,  0));
+			RevealCells(FindCellInPos(startCell,  1,  1));
+				
+			RevealCells(FindCellInPos(startCell,  0, -1));
+			RevealCells(FindCellInPos(startCell,  0,  1));
+			
+		}
+		
+		return 0;
+	}
+	
+	private Cell FindCellInPos(Cell startCell, int posX, int posY)
+	{
+		for (int i = 0; i < cells.Length; i++)
+		{
+			if(cells[i].GetColumn() == startCell.GetColumn() - posY && cells[i].GetRow() == startCell.GetRow() - posX)
+			{
+				// found a cell in set pos
+				return cells[i];
+			}
+		}
+		//did not find a cell in set pos
+		return startCell;
+	}
+	
+	
 }
--- cst250/milestone_project/MineSweeper/src/Program.cs
+++ cst250/milestone_project/MineSweeper/src/Program.cs
@@ -11,12 +11,61 @@ class Program
 		board.SetupLiveNeighbors();
 
 		//display board
+		PrintBoardForLiveGame(board);
+		
+		Console.WriteLine("BEFORE VISIT");
+		
+		board.RevealCells(board.GetCells()[5]);
+		
+		PrintBoardForLiveGame(board);
+		
+		Console.WriteLine("AFTER VISIT");
+		
 		PrintBoard(board);
 	}
 	
 	static void PrintBoardForLiveGame(Board board)
 	{
-		
+		for(int i = 0; i < board.GetSize()[0]; i++)
+		{
+			if(i == 0) Console.Write("     ");
+			Console.Write("--{0}--", i);
+		}
+		int l = 0;
+		for(int i = 0; i < board.GetCells().Length; i++)
+		{
+			//Display the board as an actual square
+			if(i % board.GetSize()[0] == 0)
+			{
+				Console.WriteLine("");
+				Console.Write("--{0}--", l);
+				l++;
+			}
+
+			if(board.GetCells()[i].GetVisited())
+			{
+				int num = -1;
+			
+				string y = board.GetCells()[i].GetLiveNeighbors().ToString();
+			
+				if (board.GetCells()[i].GetLive()) num = 0;
+				else if (board.GetCells()[i].GetLiveNeighbors() == 0) num = 1;
+				else num = 2;
+			
+				switch(num)
+				{
+					case 0: Console.Write("[ " + "X" + " ]"); break;
+					case 1: Console.Write("[ " + " " + " ]"); break;
+					case 2: Console.Write("[ " +  y  + " ]"); break;
+				}
+			}
+			else
+			{
+				Console.Write("[ " + "?" + " ]");
+			}
+			
+			
+		}
 	}
 	
 	static void PrintBoard(Board board)
@@ -37,22 +86,19 @@ class Program
 				l++;
 			}
 
-			//If square is a bomb display it as [X]
-			//else display its live neighbors value
-			if (board.GetCells()[i].GetLive())
+			int num = -1;
+			
+			string y = board.GetCells()[i].GetLiveNeighbors().ToString();
+			
+			if (board.GetCells()[i].GetLive()) num = 0;
+			else if (board.GetCells()[i].GetLiveNeighbors() == 0) num = 1;
+			else num = 2;
+			
+			switch(num)
 			{
-				Console.Write("[ " + "X" + " ]");
-			}
-			else
-			{
-				if(board.GetCells()[i].GetLiveNeighbors() == 0)
-				{
-					Console.Write("[ " + " " + " ]");
-				}else
-				{
-					Console.Write("[ " + board.GetCells()[i].GetLiveNeighbors().ToString() + " ]");
-				}
-				
+				case 0: Console.Write("[ " + "X" + " ]"); break;
+				case 1: Console.Write("[ " + " " + " ]"); break;
+				case 2: Console.Write("[ " +  y  + " ]"); break;
 			}
 		}
 	}
